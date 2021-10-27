1f610e591fef2f86351c7f268977fb9fddfc6f87
krishnachaitanya0107 <krishnachaitanya010700@gmail.com>
2021-10-27 16:22:03 +0530

    added splash screen

---
+++ app/src/main/java/com/example/space_xapp/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.space_xapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        // animation used - https://lottiefiles.com/24944-rocket-launch
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
